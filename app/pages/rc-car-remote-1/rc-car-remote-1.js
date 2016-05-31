04292b3f0df6eff1f5a11c90f2719528377c3519
Gabriel Stan <gstan@luxoft.com>
2016-05-31 15:32:22 +0300

    fist commit

---
+++ app/pages/rc-car-remote-1/rc-car-remote-1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by GStan on 31.05.2016.
+ */
+import {Page} from 'ionic-angular';
+
+
+@Page({
+    templateUrl: 'build/pages/rc-car-remote-1/rc-car-remote-1.html'
+})
+export class RcCarRemote1 {
+    constructor() {
+
+    }
+}
