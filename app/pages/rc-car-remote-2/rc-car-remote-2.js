47aaeb473997e14fb7e0eebf0513d5c27acc8829
Gabriel Stan <gstan@luxoft.com>
2016-07-05 18:40:15 +0300

    joystick 2 modifications..

--- app/pages/rc-car-remote-2/rc-car-remote-2.js
+++ app/pages/rc-car-remote-2/rc-car-remote-2.js
@@ -6,14 +6,52 @@ import {Page} from 'ionic-angular';
 @Page({
     templateUrl: 'build/pages/rc-car-remote-2/rc-car-remote-2.html'
 })
+
 export class RcCarRemote2Page {
 
-    onPageWillEnter(){
-
-    }
-
     constructor() {
-
+        this.mouseHold = false;
+        this.notMoving = true;
+        this.x;
+        this.y;
+        this.posx;
+        this.posy
     }
 
+    mouseDown(e) {
+        console.debug(e);
+        this.mouseHold = true
+    }
+
+    mouseUp(e) {
+        // console.debug(e);
+        this.mouseHold = false;
+        this.notMoving = true
+    }
+
+    mouseMove(e) {
+        console.debug('mouse DOWN - ' + this.mouseHold);
+
+        if (this.mouseHold) {
+             console.debug(e);
+
+            if(notMoving){
+
+                this.x = e.offsetX;
+                this.y = e.offsetY;
+
+                this.notMoving = false
+            }
+
+            this.posx = e.pageX - this.x;
+            this.posy = e.pageY - this.y;
+            e.target.style.left = this.posx + 'px';
+            e.target.style.top = this.posy + 'px'
+        }
+    }
+
+
+    ngAfterViewInit() {
+        console.debug(this.mouseHold)
+    }
 }
